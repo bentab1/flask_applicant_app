45f8850bbdea63aa1508972715967b314536c220
paycare <137165467+bentab1@users.noreply.github.com>
2025-01-19 18:00:32 +0000

    commit

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ def generate_random_code(length=6):
     return random.randint(start, end)
 
 
-@app.route('/application-form', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     error_message = None
     role = None
